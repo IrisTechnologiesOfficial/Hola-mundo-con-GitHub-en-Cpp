b29c5f10813913aef1eb8244e2d44fd823c70946
PabloAprende++ <52809319+PabloAprende@users.noreply.github.com>
2019-07-13 12:41:41 -0300

    Create codigo.cpp

---
+++ codigo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hola mundo de parte de Pablo para GitHub el 13/07/2019!";
+    cout<<"y tambien de parte de: ";
+
+    getch();
+    return 0;   
+}
