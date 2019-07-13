d2ed638912560acbeacf82b068b3eaec29847ddd
IrisTechnologiesOfficial <40865154+IrisTechnologiesOfficial@users.noreply.github.com>
2019-07-13 17:51:03 +0200

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <conio.h>
-
-using namespace std;
 
 int main()
 {
-    cout<<"Hola mundo de parte de Pablo para GitHub el 13/07/2019!";
-    cout<<"y tambien de parte de: ";
-
-    getch();
+    std::cout << "Hola mundo de parte de Pablo para GitHub el 13/07/2019!" << std::endl;
+    std::cout << "y tambien de parte de: " std::endl;
+    
+    char c;
+    std::cin >> c;
     return 0;   
 }
